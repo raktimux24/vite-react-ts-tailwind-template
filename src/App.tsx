1452a8c1b525aaafe38e6f733f7327131e173bbf
Raktim Chatterjee <raktimux24@gmail.com>
2025-02-15 04:52:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <TypingSpeedTest />
       <footer className="fixed bottom-0 w-full py-4 bg-black/20 backdrop-blur-lg text-center text-sm text-white/80">
-        Copyright © 2025   |   LittleBigDots | Made with ❤️ in Bengaluru, India
+        Copyright © 2025   |   LittleBigDots India | Made with ❤️ in Bengaluru, India
       </footer>
     </>
   );
